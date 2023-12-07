2a0aee9494a0f522376917d950b879b1f821b42d
Staravoy <85824511+Staravoy@users.noreply.github.com>
2023-12-07 17:51:15 +0200

    зробив статистику

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,12 @@
 import react from 'react'
 // локальні імпорти
 import Profile from "./Profile/Profile";
+import Statistics from "./Statistics/Statistics"
+// стилі
 import s from './App.css'
 // БД
 import User from '../Data/user.json'
+import data from '../Data/data.json'
 
 export const App = () => {
   return (
@@ -16,8 +19,10 @@ export const App = () => {
         avatar={User.avatar}
         stats={User.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
     </div>
-    
   );
 };
 
