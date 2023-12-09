40f62aa36b25c91be19bfc2ec3cce03656884698
Staravoy <85824511+Staravoy@users.noreply.github.com>
2023-12-09 11:35:12 +0200

    for deploy

--- src/index.js
+++ src/index.js
@@ -8,3 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+<BrowserRouter basename="https://github.com/Staravoy/-goit-react-hw-01-components-tato.git">
+  <App />
+</BrowserRouter>
